4a1f8c8031aa64bed5d61687db62e958183f72cd
Keshav Sharma <keshavsharmaog@Keshavs-MacBook-Air.local>
2025-10-21 18:07:42 +0530

    removed profile redundant folder

--- src/app/profile/[userId]/page.tsx
+++
@@ -1,6 +0,0 @@
-export default async function UserDetails(
-    {params}: {params: Promise<{userId: string}>}
-) {
-    const userId = (await params).userId;
-    return <h1>Details about User, with id == {userId}</h1>
-}
